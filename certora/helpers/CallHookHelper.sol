8e0a503ea69e408edd92f92b185cff643c7c0b60
Andrew Ferraiuolo <andrew@certora.com>
2024-10-10 09:53:38 +0100

    Delete callhook helper

--- certora/helpers/CallHookHelper.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract CallHookHelper {
-    // This funciton is just a way to save msg.sender and msg.value
-    // from unresolved calls. This gets routed to a CVL function.
-    function HarnessCallHook() external {}
-}
